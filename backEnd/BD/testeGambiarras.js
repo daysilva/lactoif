68f0a4763fad10b221c085c8a8c1b0dbbe2bc50f
daysilva <adaiane73@gmail.com>
2022-12-16 21:44:38 -0300

    testefor

--- backEnd/BD/testeGambiarras.js
+++ backEnd/BD/testeGambiarras.js
@@ -1,51 +1,64 @@
 const connection = require('./conexao')
 
-const CadastrarAuxiliar_producao = async (nomeAuxiliar, nomeProduto) => {
-    // percorrer todos os elementos que estão vindo
+const CadastrarAuxiliar_producao = async (nomeAuxiliar, n_producao) => {
 
-    while (true) {
-        let i = 0
-        // let x = 0
-        const valor = null
+    // descobrir o id da producao
+   const idProducao = `select id from producao where n_producao = "${n_producao}"`
+   const id_prod = await connection.execute(idProducao)
+   console.log(id_prod[0])
 
-        if (nomeAuxiliar[i] != undefined || nomeProduto[i] != undefined) {
+    for (let i = 0; i < nomeAuxiliar.length; i++) {
 
-            if (nomeAuxiliar[i] != undefined) {
-                
-            }
-
-
-
-
-             // asntes dar um select para descobrir qual o id do nome desse tecnico q mandaram
+         // asntes dar um select para descobrir qual o id do nome desse tecnico q mandaram
         const id_auxiliar = `select id from auxiliares where nome = "${nomeAuxiliar[i]}"`
         const id_aux = await connection.execute(id_auxiliar)
 
-        // descobrir o id do produto
-        const id_produto = `select id from produto where nome = "${nomeProduto[i].nome}"`
-        const id_prod = await connection.execute(id_produto)
-        
+        const id_Auxiliar2 = await id_aux[0]
 
-        // [{nome: }]
-        // vai estar dentro de um for
-        const a = 'INSERT INTO auxiliar_produto (id_auxiliar, id_produto) VALUES (?, ?)'
-        const auxiliar = await connection.execute(a,
-            [id_aux[0][0].id, id_prod[0][0].id])
-
-            i++
+        for (let x = 0; x < id_prod[0].length; x++) {
+            
+             [{nome: id_aux}]
+            // vai estar dentro de um for
+            console.log(`idAux: ${id_Auxiliar2[i].id} idPro ${id_prod[0][x].id}`)
+             const a = 'INSERT INTO auxiliar_produto (id_auxiliar, id_producao) VALUES (?, ?)'
+               const auxiliar = await connection.execute(a,
+                   [id_aux[0][0].id, id_prod])
+            
         }
+    }
 
-            }
 
-  
+
+   // [{nome: }]
+   // vai estar dentro de um for
+//    const a = 'INSERT INTO auxiliar_produto (id_auxiliar, id_producao) VALUES (?, ?)'
+//    const auxiliar = await connection.execute(a,
+//        [id_aux[0][0].id, id_prod])
 }
 
 
+/* const exibirAuxi = async (nomeAuxiliar) => {
 
-const aux = ["allan", "denys"]
-const prod = [{nome: "quejo", quantidade: "2"}]
+    const selectTextoInicio = 'select'
 
 
-CadastrarAuxiliar_producao(aux, prod)
+    for (let i = 0; i < nomeAuxiliar.length; i++) {
+        selectTextoInicio += ' (select id from auxiliares where nome = ' +  nomeAuxiliar[i] + '"),'
+    }
+
+    selectTextoInicio += ' from auxiliares'
+    console.log(selectTextoInicio) */
+
+    //  const id_aux = await connection.execute(selectTextoInicio)
+    //  console.log(id_aux[0])
+// }
+
+
+const aux = ["allan", "Ricardo"]
+const prod = "1/2022"
+
+
+// CadastrarAuxiliar_producao(aux, prod)
+exibirAuxi(aux)
 
 
